944cdc38da3b9c84b671f8a0b099220f1128cbae
ALBA-Girly <Albaumeh@gmail.com>
2025-11-22 10:54:00 +0100

    Add files via upload

---
+++ bustdownthotiana.py
@@ -0,0 +1,13 @@
+print("These are my best friends birthday and my name is ALba my bestest friend among all is Sofia and my least bestest friend is Ella andd Alma not so much but emily is also my bestest friend but sofia is first "
+)
+birthday1 = "2014-03-04"   # March 4, 2014
+birthday2 = "2014-11-07"   # November 7, 2014
+birthday3 = "2014-10-01"   # October 1, 2014
+birthday4 = "2014-02-14"   # February 14, 2014
+birthday5 = "2014-08-29"   # August 29, 2014
+
+print("This is Alba's birthday:", birthday1)
+print("This is Sofia's birthday:", birthday2)
+print("This is Alma's birthday:", birthday3)
+print("This is Emily's birthday:", birthday4)
+print("This is Ella's birthday:", birthday5)
